651efcad6319b876a56a85d6da181ad329dea05b
Zelin Xia <scyzx5@nottingham.edu.cn>
2024-11-09 12:47:46 +0800

    Add title for our cartoon

--- PlatformerPlain/src/main/java/com/example/platformerplain/Main.java
+++ PlatformerPlain/src/main/java/com/example/platformerplain/Main.java
@@ -3,7 +3,6 @@ package com.example.platformerplain;
 import com.example.platformerplain.map.EntityFactory;
 import com.example.platformerplain.EntityType;
 
-
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Node;
@@ -12,6 +11,8 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -39,6 +40,17 @@ public class Main extends Application {
         Rectangle bg = new Rectangle(BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
         levelWidth = LevelData.Level1[0].length() * TILE_SIZE;
 
+        // Create a text element for the title
+        Text title = new Text("Try to get the goal");
+        title.setFont(new Font(36));  // Set the font size
+        title.setFill(Color.YELLOW);  // Set the text color to yellow
+        // Center the text horizontally by calculating its X position
+        double textWidth = title.getLayoutBounds().getWidth();
+        title.setX((BACKGROUND_WIDTH - textWidth) / 2);
+        title.setY(40);  // Position Y for visibility at the top middle
+
+        uiRoot.getChildren().add(title);
+
         for (int i = 0; i < LevelData.Level1.length; i++) {
             String line = LevelData.Level1[i];
             for (int j = 0; j < line.length(); j++) {
@@ -46,35 +58,35 @@ public class Main extends Application {
                     case '0':
                         break;
                     case '1':
-                        Entity platform = createEntity(EntityType.PLATFORM,j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                        Entity platform = createEntity(EntityType.PLATFORM, j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE);
                         entitymap.add(platform);
                         break;
                     case '9':
-                        Entity goal = createEntity(EntityType.GOAL,j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                        Entity goal = createEntity(EntityType.GOAL, j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE);
                         entitymap.add(goal);
                         break;
                 }
             }
         }
-        player = createEntity(EntityType.PLAYER,PLAYER_START_X, PLAYER_START_Y, PLAYER_SIZE, PLAYER_SIZE);
+
+        player = createEntity(EntityType.PLAYER, PLAYER_START_X, PLAYER_START_Y, PLAYER_SIZE, PLAYER_SIZE);
         player.node().translateXProperty().addListener((obs, old, newValue) -> {
             int offset = newValue.intValue();
             if (offset > BACKGROUND_WIDTH / 2 && offset < levelWidth - BACKGROUND_WIDTH / 2) {
                 gameRoot.setLayoutX(-(offset - BACKGROUND_WIDTH / 2));
             }
         });
+
         appRoot.getChildren().addAll(bg, gameRoot, uiRoot);
 
         moveLogic = new Move(player, entitymap, levelWidth, keys);
     }
-
 
     private Entity createEntity(EntityType type, int x, int y, int w, int h) {
         Entity entity = EntityFactory.createEntity(type, x, y, w, h);
         gameRoot.getChildren().add(entity.node());
         return entity;
     }
-
 
     @Override
     public void start(Stage primaryStage) {
