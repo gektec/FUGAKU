4a00e4b22d4afac20e530f837ed936d58b133ef7
Zelin Xia <scyzx5@nottingham.edu.cn>
2024-12-08 14:56:55 +0800

    Change the feature of button "start game"

--- PlatformerPlain/src/main/java/com/example/platformerplain/Controller/MenuScreenController.java
+++ PlatformerPlain/src/main/java/com/example/platformerplain/Controller/MenuScreenController.java
@@ -1,6 +1,8 @@
 package com.example.platformerplain.Controller;
 
 import com.example.platformerplain.Main;
+import com.example.platformerplain.Screen.LevelSelectScreen;
+import com.example.platformerplain.Screen.MenuScreen;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -69,12 +71,10 @@ public class MenuScreenController {
 
     // Handle the start game button click event
     @FXML
-    void handleStartGame() {
-        if (primaryStage != null) {
-            Main.getInstance().startGame(primaryStage);
-        } else {
-            System.err.println("Primary stage is not set.");
-        }
+    private void handleStartGame() {
+        // Create a StartScreen instance and pass the main stage
+        LevelSelectScreen selectScreen = new LevelSelectScreen();
+        selectScreen.show(primaryStage);  // deliver currentStage
     }
 
     @FXML
